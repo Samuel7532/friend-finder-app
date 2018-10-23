cf0e3258fa899a1976b2430d9d7301a5fc104468
Samuel Almanzar <samalmanzar@Samuels-MacBook-Air.local>
2018-10-23 16:01:19 -0400

    itial files made

---
+++ server.js
@@ -0,0 +1,12 @@
+// Dependencies
+var express = require("express");
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
