543b1c7f56fcb4a781b8493cf56a6bae5893ca5d
Samuel Almanzar <samalmanzar@Samuels-MacBook-Air.local>
2018-10-23 16:31:13 -0400

    html

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ var PORT = process.env.PORT || 8080;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/:", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+  
