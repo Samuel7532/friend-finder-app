d04b914b4a2a81926e263a62554e8f3e3e068a61
Samuel Almanzar <samalmanzar@Samuels-MacBook-Air.local>
2018-10-29 18:51:51 -0400

    merge

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friends = [
+    {
+        name: "bob",
+        photo: "",
+        scores: [3,2,5,3,2,3,4,5,1,3],
+
+    }
+]
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Dependencies
 var express = require("express");
-
+var bodyParser = require("body-parser");
 // Create an instance of the express app.
 var app = express();
 
@@ -9,14 +9,18 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-  
-  app.get("/:", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+app.listen(PORT, function(){
+    console.log("LIstening on " + PORT);
+});
   
